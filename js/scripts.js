1e94f230babe26bc349cc503efcc29dd08f4b0bf
MegatronTed <santa@northpole.com>
2019-09-23 15:18:48 +0300

    add content to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
-var malenames=["Kwasi","Kwadwo","kwadena","Kwaku","Yaw","Kofi","Kwame"];
-var femalenames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var daysoftheweek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
-function akanName(){
-    var month=parseInt(document.getElementById("Month").value);
-    var day=parseInt(document.getElementById("Day").value);
-    var year=parseInt(document.getElementById("Year").value);
-    var Gender=getGender();
-
-var gender = document.getElementById("choice").value;
+var maleNames=["Kwasi","Kwadwo","kwadena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var daysOfTheweek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
+function akan(){
+    var Month=parseInt(document.getElementById("month").value);
+    var Day=parseInt(document.getElementById("day").value);
+    var Year=parseInt(document.getElementById("year").value);
+    var gender=getGender();
 var date=new Date(Year +"/"+Month+"/"+Day);
-var dayborn= date.getDay();
+var dayBorn= date.getDay();
 var akanName;
-   if(gender==="male"){
-       akanName =malenames[dayBorn];
+if(gender ==="male"){
+       akanName = maleNames[dayBorn];
    }
    else{
-       akanName=femalenames[dayBorn];
+       akanName = femaleNames[dayBorn];
    }
-   alert("You were born on"+daysoftheweek[dayBorn]+"And your Akan name is"+akanName);
+   alert("You were born on "+daysOfTheweek[dayBorn]+" and your Akan name is "+akanName);
 }
-   function getNames(){
-       var gender=documentent.getElementsByName("gender");
-       for(i=0;i<gender.length;i++){
+   function getGender(){
+       var gender = document.getElementsByName("gender");
+       for(i= 0; i < gender.length; i++){
            if(gender[i].checked){
                return(gender[i].value)
            }
